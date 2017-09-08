e9a8b1cabae2fb5a73754d3bbf522a347bcf66bb
NessaDevelops <nessa.develops@gmail.com>
2017-09-08 16:12:42 -0500

    Styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,16 +10,17 @@ class Home extends Component {
         <div className="content">
           <h1>Looking for a job?</h1>
           <h5>Take the next step in your career</h5>
-          <div>
-            
-          </div>
+          <form className="search-container">
+            <input className="search__input" type="text" placeholder="Job Title, Company or Keyword" />
+            <button className="search__button" type="submit">Search</button>
+          </form>
           <div className="grid">
             <figure className="effect">
               <img src={require("../../imgs/1.jpg")} alt="img01"/>
               <figcaption>
                 <h2><span>Professional</span></h2>
                 <p>View Jobs</p>
-                <a href="#">View more</a>
+                <a href="">View more</a>
               </figcaption>			
             </figure>
             <figure className="effect">
@@ -27,7 +28,7 @@ class Home extends Component {
               <figcaption>
                 <h2>Customer <span>Service</span></h2>
                 <p>View Jobs</p>
-                <a href="#">View more</a>
+                <a href="">View more</a>
               </figcaption>			
             </figure>
             <figure className="effect">
@@ -35,7 +36,7 @@ class Home extends Component {
               <figcaption>
                 <h2>Customer <span>Service</span></h2>
                 <p>View Jobs</p>
-                <a href="#">View more</a>
+                <a href="">View more</a>
               </figcaption>			
             </figure>
             <figure className="effect">
@@ -43,7 +44,7 @@ class Home extends Component {
               <figcaption>
                 <h2>Customer <span>Service</span></h2>
                 <p>View Jobs</p>
-                <a href="#">View more</a>
+                <a href="">View more</a>
               </figcaption>			
             </figure>
           </div>
