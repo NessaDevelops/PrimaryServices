1348b509f4b61d9b2d42205252932f6d1bbdaf8e
NessaDevelops <nessa.develops@gmail.com>
2017-09-08 17:46:27 -0500

    add theme css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import './styles/theme.css';
 import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
