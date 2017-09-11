b6f4eb0cd614b90ddcf4dc7521a44e5c25eadfe7
NessaDevelops <nessa.develops@gmail.com>
2017-09-11 16:43:56 -0500

    view job skeleton

--- src/pages/Jobs/ViewJob/ViewJob.js
+++ src/pages/Jobs/ViewJob/ViewJob.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import NavBar from '../../../components/NavBar';
 import './ViewJob.css';
 
@@ -7,6 +8,55 @@ class ViewJob extends Component {
     return (
       <div className="ViewJob">
         <NavBar></NavBar>
+        <h2>Professional</h2>
+          <div className="container">
+            <section>
+              <img src={require("../../../imgs/Icons/back.png")} /><a>Back to results</a>
+              <div className="job">
+                <h5>Bilingual Customer Service</h5>
+                <div className="job-header">
+                  <img src={require("../../../imgs/Icons/location.png")} /><p>Houston, TX</p>
+                  <img src={require("../../../imgs/Icons/clock.png")} /><p>Posted Today</p>
+                </div>
+                <p>Ready for the next step in your Customer Service career? Primary Services is conducting interviews for Bilingual Tier I customer service roles with an up and coming green energy company.  In this role, you will be providing high-level professional customer service to customers in North and South America! Apply Immediately!</p>
+                <p><br/>Ready to work in a call center that isn’t just a dead end job? Apply now to join a rapidly growing industry with large potential for career growth!</p>
+                <Link to="/viewjob">
+                  <p>View Job</p>
+                </Link>
+              </div>
+            </section>
+            <aside>
+              <h5>Related Jobs</h5>
+              <div className="job">
+                <h5>Accountant III</h5>
+                <p>Houston, TX</p><p>Posted Today</p>
+                <Link to="/viewjob">
+                  <p>View Job</p>
+                </Link>
+              </div>
+              <div className="job">
+                <h5>Financial Account I</h5>
+                <p>Houston, TX</p><p>Posted Today</p>
+                <Link to="/viewjob">
+                  <p>View Job</p>
+                </Link>
+              </div>
+              <div className="job">
+                <h5>Staff Accountant</h5>
+                <p>Houston, TX</p><p>Posted Today</p>
+                <Link to="/viewjob">
+                  <p>View Job</p>
+                </Link>
+              </div>
+              <div className="job">
+                <h5>Accountant - Advanced</h5>
+                <p>Houston, TX</p><p>Posted Today</p>
+                <Link to="/viewjob">
+                  <p>View Job</p>
+                </Link>
+              </div>
+            </aside>
+          </div>
       </div>
     );
   }
