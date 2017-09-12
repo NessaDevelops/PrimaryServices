6c5ba3c01b189668bf065daf2634fce9f6c7879c
NessaDevelops <nessa.develops@gmail.com>
2017-09-12 12:43:25 -0500

    Partial theme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import './styles/theme.css';
 import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
