ec698c03aebfc85d0d47969139d95caab54195ca
NessaDevelops <nessa.develops@gmail.com>
2017-09-12 17:31:15 -0500

    Double search bar

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -6,7 +6,11 @@ class SearchBar extends Component {
   render() {
     return (
       <form className="search-container">
-        <input type="text" placeholder="Job Title, Company or Keyword" />
+        <div className="input-container">
+          <input type="text" placeholder="Job Title, Company or Keyword" />
+          <img src={require("../../imgs/Icons/location.png")} />
+          <input type="text" placeholder="City & State or ZIP" />
+        </div>
         <Link to="/search">
           <button type="submit" />
         </Link>
